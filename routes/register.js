583407394bf4ab464d7ea5e5e8c42007a71bcdbf
wang.chunyong <wchuny@cn.ibm.com>
2016-09-04 21:07:18 +0800

    register function bug fixed.

--- routes/register.js
+++ routes/register.js
@@ -22,7 +22,9 @@ router.post('/', function(req, res) {
     var user_password = req.body.user_password;
 	var user_name = req.body.user_name;
 	console.log(user_id);
-	db.insert({ _id: "users_" + user_id, "user_name":user_name, "user_id":user_id, "user_password": user_password, "type":"user", processes: new Array()}, function(err, data) {
+	db.insert({ _id: "users_" + user_id, "user_name":user_name, "user_id":user_id, "user_password": user_password, "type":"user", 
+			"processes": [{"process_id": "process_sample","process_name": "Sample Process","process_authority": "viewer"}],
+			"user_pref_default_view":"kanban","user_pref_default_process": "process_sample"}, function(err, data) {
 		console.log("Error:", err);
 		console.log("Data:", data);
 		var resultdata = {};
