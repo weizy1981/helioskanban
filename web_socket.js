9942a33e63a7bc3ab9e6c76e958429b5a2279b48
wang.chunyong <wchuny@cn.ibm.com>
2016-09-28 08:58:27 +0800

    test.

--- web_socket.js
+++ web_socket.js
@@ -2,8 +2,13 @@ var WebSocket = function(io,socket) {
 	socket.on('disconnect', function(){
 		//console.log('User disconnected');
 	});
+
+	socket.on('group1', function (data) {
+        socket.join('group1');
+	});
+
 	socket.on('taskedit',function(userObj){
-		io.emit('messages', userObj);
+		io.sockets.emit('messages', userObj);
 	});
 };
 module.exports = WebSocket;
